d53cadbfbebc63b90cb258fe87a70836f70c2904
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-04-04 11:50:21 -0400

    addNewAnimal

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { addNewAnimal } from "./src/animals.js";
+
 import express  from "express";
 import cors from 'cors';
 
@@ -7,7 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json()) // tells express that our POSTS will be JSON
 
-app.get('/animals',getAllAnimals)
+// app.get('/animals',getAllAnimals)
 app.post('/animals',addNewAnimal)
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}...`)
