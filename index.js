92e17db5c39b479718c50e19cdac0e3a9fb7c72d
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-04-04 11:35:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express  from "express";
+import cors from 'cors';
+
+const PORT = 3002
+
+const app = express()
+app.use(cors())
+app.use(express.json()) // tells express that our POSTS will be JSON
+
+app.get('/animals',getAllAnimals)
+app.post('/animals',addNewAnimal)
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}...`)
+})
